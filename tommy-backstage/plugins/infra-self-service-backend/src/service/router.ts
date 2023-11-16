7a1379a4b6176db2c4132c3a3b803e6e9bbba17a
Tran Thang <tranthang.dev@gmail.com>
2023-11-16 17:58:14 +0700

    update

--- tommy-backstage/plugins/infra-self-service-backend/src/service/router.ts
+++ tommy-backstage/plugins/infra-self-service-backend/src/service/router.ts
@@ -2,6 +2,7 @@ import { errorHandler } from '@backstage/backend-common';
 import express from 'express';
 import Router from 'express-promise-router';
 import { Logger } from 'winston';
+import { handler } from './domainfunction/main'
 
 export interface RouterOptions {
   logger: Logger;
@@ -17,8 +18,14 @@ export async function createRouter(
 
   router.get('/health', (_, response) => {
     logger.info('PONG!');
-    response.json({ status: 'ok' });
+    response.json({ status: 'okie' });
   });
+
+  router.post('/createinfra', (request, response) => {
+    handler(request.body);
+    response.json({ status: 'okie' });
+  })
+
   router.use(errorHandler());
   return router;
 }
