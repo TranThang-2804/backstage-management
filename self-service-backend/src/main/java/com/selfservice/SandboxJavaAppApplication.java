b26b47a6561521512d22d009506f33546c4fc32c
Tran Thang <tranthang.dev@gmail.com>
2023-11-16 15:17:55 +0700

    set up backend plugin and added it to docker compose
    ;
    '

--- self-service-backend/src/main/java/com/selfservice/SandboxJavaAppApplication.java
+++ self-service-backend/src/main/java/com/selfservice/SandboxJavaAppApplication.java
@@ -1,4 +1,4 @@
-package com.confortdelgro;
+package com.selfservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- self-service-backend/src/main/java/com/selfservice/controller/HomeController.java
+++ self-service-backend/src/main/java/com/selfservice/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.confortdelgro.controller;
+package com.selfservice.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
